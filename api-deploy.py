c838b7f437ae722b6e5d4cbad53c2c643cdddcb2
Kazuki Tsunemi <ksilverwall@gmail.com>
2019-10-14 12:31:38 +0900

    Update api-deploy.py

---
+++ api-deploy.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import subprocess
+
+BUCKET_NAME = 'thankshell-api'
+
+ENV_PARAMS = {
+    'production': {
+        'stack-name': 'thankshell-api',
+    },
+    'staging': {
+        'stack-name': 'thankshell-api-stg',
+    },
+}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-e', '--environment',
+        choices=ENV_PARAMS.keys(),
+        default='staging',
+    )
+    args = parser.parse_args()
+
+    os.chdir('../thankshell-api-2')
+
+    params = ENV_PARAMS[args.environment]
+
+    subprocess.run([
+        'sam', 'package',
+        '--output-template-file', 'packaged.yaml',
+        '--s3-bucket', BUCKET_NAME,
+    ])
+
+    subprocess.run([
+        'sam', 'deploy',
+        '--template-file', 'packaged.yaml',
+        '--stack-name', params['stack-name'],
+        '--capabilities', 'CAPABILITY_IAM',
+    ])
--- api-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-BUCKET_NAME='thankshell-api'
-
-cd ../thankshell-api
-sam package --output-template-file packaged.yaml --s3-bucket ${BUCKET_NAME}
-sam deploy --template-file packaged.yaml --stack-name thankshell-api --capabilities CAPABILITY_IAM
